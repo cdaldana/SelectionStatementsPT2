d7e07a24031296324a2128214dbb0cebaf821b7a
Christian Aldana <cdaldana90@gmail.com>
2020-08-25 14:57:00 -0500

    part 2 exercise done

---
+++ SelectionStatementsPT2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SelectionStatementsPT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); //evaluate to a string
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("That is a tough subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("I stay away from science, it's so hard!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is an interesting subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I have never taken that subject before. {subject} sounds fun!");
+                    break;
+            }
+        }
+    }
+}
